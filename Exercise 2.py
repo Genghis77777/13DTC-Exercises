d1d723ee277f07eace16b0b87d0f96ae762f610e
Genghis77777 <joshua.kan@outlook.co.nz>
2022-02-08 11:52:47 +1300

    Exercises 1-3 Complete

---
+++ Exercise 2.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter another another number: "))
+num4 = int(input("Enter another another another number: "))
+
+print("The sum of your numbers is:")
+print(num1 + num2 + num3 + num4)
